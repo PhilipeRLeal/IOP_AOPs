240f2603680d0ea6385843aaa26ba6396f05c2a8
PhilipeRLeal <leal.philipe@gmail.com>
2021-09-02 17:08:13 -0300

    test suits updated

--- IOP_AOPs/tests/QAA_analysis_tests.py
+++ IOP_AOPs/tests/QAA_analysis_tests.py
@@ -1,35 +1,38 @@
 import numpy as np
 import pandas as pd
+from unittest import TestCase
 
 from .IOP_AOPs.QAA_analysis import apply_QAA
 
+class Testgcdistance(TestCase):
+    def test_apply_QAA(self):
+        """
+        This is a function to evaluate the QAA algorithm
 
-def test_apply_QAA():
-    """
-    This is a function to evaluate the QAA algorithm
-    
-    """
-    try:
-        N_samples = 10
+        """
+        try:
+            N_samples = 10
 
-        Bandas = [412, 443, 489, 510, 555, 670]
+            Bandas = [412, 443, 489, 510, 555, 670]
 
-        size = (N_samples, len(Bandas)) / 1000
+            size = (N_samples, len(Bandas)) / 1000
 
-        Random_data = np.random.randint(low=0,
-                                        high=800,
-                                        size=size
-                                        )
+            Random_data = np.random.randint(low=0,
+                                            high=800,
+                                            size=size
+                                            )
 
-        Rrs_Data = pd.DataFrame(Random_data, columns=Bandas)
+            Rrs_Data = pd.DataFrame(Random_data, columns=Bandas)
 
-        QAA_Results = apply_QAA(Rrs_Data)
+            QAA_Results = apply_QAA(Rrs_Data)
 
-        for k, v in QAA_Results.items():
-            print(str(k), '\n' * 3, '-' * 50,
-                  '\n', v, '\n' * 3)
+            for k, v in QAA_Results.items():
+                print(str(k), '\n' * 3, '-' * 50,
+                      '\n', v, '\n' * 3)
 
-        assert isinstance(Rrs_Data, pd.DataFrame)
+            R = isinstance(Rrs_Data, pd.DataFrame)
 
-    except BaseException:
-        assert False
+        except BaseException:
+            R = False
+
+        self.assertTrue(R)
--- IOP_AOPs/tests/response_function_analysis_tests.py
+++ IOP_AOPs/tests/response_function_analysis_tests.py
@@ -1,95 +1,96 @@
 import pandas as pd
 import seaborn as sns
+from unittest import TestCase
 
 from IOP_AOPs import spectral_response_functions as SRF
-
 from SRF.response_function_analysis import central_wavelength_function
 del SRF
 
-
 def get_OLI_response_data():
-    """
-    Basic function for acquisition of the OLI data
-    to be applied by the 'test_response_function'
+        """
+        Basic function for acquisition of the OLI data
+        to be applied by the 'test_response_function'
 
-    """
+        """
 
-    filepath = r'data/Ball_BA_RSR.v1.xlsx'
+        filepath = r'data/Ball_BA_RSR.v1.xlsx'
 
-    all_dfs = pd.read_excel(filepath, sheet_name=None)
+        all_dfs = pd.read_excel(filepath, sheet_name=None)
 
-    General = pd.read_excel(filepath, sheet_name='GENERAL', header=[0, 1],
+        General = pd.read_excel(filepath, sheet_name='GENERAL', header=[0, 1],
 
-                            )
+                                )
 
-    sheets = all_dfs.keys()
+        sheets = all_dfs.keys()
 
-    print(sheets)
+        print(sheets)
 
-    # concat all sheets at once:
+        # concat all sheets at once:
 
-    # df = pd.concat(pd.read_excel(workbook_url,
-    #                sheet_name=None), ]
-    #                ignore_index=True
-    #               )
+        # df = pd.concat(pd.read_excel(workbook_url,
+        #                sheet_name=None), ]
+        #                ignore_index=True
+        #               )
 
-    df = pd.concat(
-        [
-            all_dfs[key] for key in sheets if key not in [
-                'GENERAL', 'Pan']], names=[
-                    'Band_name', 'id'], keys=[
-                        key for key in sheets if key not in [
-                            'GENERAL', 'Pan']]).set_index(
-                                'Wavelength', append=True)
+        df = pd.concat(
+            [
+                all_dfs[key] for key in sheets if key not in [
+                    'GENERAL', 'Pan']], names=[
+                        'Band_name', 'id'], keys=[
+                            key for key in sheets if key not in [
+                                'GENERAL', 'Pan']]).set_index(
+                                    'Wavelength', append=True)
 
-    df = df.reset_index(level=1, drop=True)
+        df = df.reset_index(level=1, drop=True)
 
-    return df, General
+        return df, General
 
+class Testgcdistance(TestCase):
+    def test_response_function(self):
+        """
+        This is a test for the central_wavelength_function
+        by means of the OLI remote sensor response function.
 
-def test_response_function():
-    """
-    This is a test for the central_wavelength_function
-    by means of the OLI remote sensor response function.
+        """
 
-    """
+        try:
 
-    try:
+            df, General = get_OLI_response_data()
 
-        df, General = get_OLI_response_data()
+            # Plotting the sensitivity of each Band
 
-        # Plotting the sensitivity of each Band
+            palette = sns.color_palette("rocket_r")
 
-        palette = sns.color_palette("rocket_r")
+            sns.relplot(
+                data=df.reset_index(),
+                x="Wavelength",
+                y='BA RSR [watts]',
+                col="Band_name",
+                col_wrap=3,
+                kind="line", palette=palette,
+                height=5, aspect=.75,
+                facet_kws=dict(sharex=True),
+            )
 
-        sns.relplot(
-            data=df.reset_index(),
-            x="Wavelength",
-            y='BA RSR [watts]',
-            col="Band_name",
-            col_wrap=3,
-            kind="line", palette=palette,
-            height=5, aspect=.75,
-            facet_kws=dict(sharex=True),
-        )
+            # Central wavelength
 
-        # Central wavelength
+            RFB = {}
 
-        RFB = {}
+            for band in df.index.get_level_values('Band_name').unique():
 
-        for band in df.index.get_level_values('Band_name').unique():
+                RFB_i = central_wavelength_function(df, band_name=band)
+                print('central_wavelength: ', RFB_i)
 
-            RFB_i = central_wavelength_function(df, band_name=band)
-            print('central_wavelength: ', RFB_i)
+                RFB[band] = RFB_i
 
-            RFB[band] = RFB_i
+            RFB = pd.Series(RFB, name='Center Wavelength [nm] - estimated')
+            RFB.index.name = 'Bandname'
 
-        RFB = pd.Series(RFB, name='Center Wavelength [nm] - estimated')
-        RFB.index.name = 'Bandname'
+            Comparison = General.loc[:, 'Band'].merge(RFB, on='Bandname')
 
-        Comparison = General.loc[:, 'Band'].merge(RFB, on='Bandname')
+            R = isinstance(Comparison, pd.DataFrame)
 
-        assert isinstance(Comparison, pd.DataFrame)
+        except BaseException:
+            R = False
 
-    except BaseException:
-        assert False
+        self.assertTrue(R)
